d59024bdfd5a12f6fecf3a5a5d56c39df567020e
Ruiyot Abby <ruiyotabby@gmail.com>
2023-09-24 11:17:52 +0300

    Implement retreiving anecdotes from the server using react query

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,27 +1,35 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { getAll } from './requests'
 
 const App = () => {
-
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAll
+  })
+  console.log(JSON.parse(JSON.stringify(result)));
+
+  if (result.isLoading) {
+    return <div>Loading data...</div>
+  }
+  if (result.isError && result.error.message === 'Network Error') {
+    return <div>Anecdotes service not available due to problems in the server</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
+
       {anecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
